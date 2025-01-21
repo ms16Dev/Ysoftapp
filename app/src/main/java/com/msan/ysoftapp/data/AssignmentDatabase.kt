5bc6b665efd26b634db9ead961964bf5c27fefd1
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-01-22 02:28:31 +0300

    add type Converters for the DB

--- app/src/main/java/com/msan/ysoftapp/data/AssignmentDatabase.kt
+++ app/src/main/java/com/msan/ysoftapp/data/AssignmentDatabase.kt
@@ -3,12 +3,14 @@ package com.msan.ysoftapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.msan.ysoftapp.data.entity.AssignmentEntity
 
 @Database(
     entities = [AssignmentEntity::class],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class AssignmentDatabase: RoomDatabase() {
 
     abstract val dao: AssignmentDao
